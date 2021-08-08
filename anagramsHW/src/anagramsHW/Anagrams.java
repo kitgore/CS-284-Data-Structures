246d20549f8260e34d800042baf33fca938c5c7d
ben <bengriepp@gmail.com>
2021-08-08 17:08:24 -0400

    Commenting

--- anagramsHW/src/anagramsHW/Anagrams.java
+++ anagramsHW/src/anagramsHW/Anagrams.java
@@ -11,7 +11,7 @@ import java.util.Map;
 /**
  * @author Benjamin Griepp
  * 
- * Grade:
+ * Grade: 100/100
  * 
  * Sorts words into hashtable by their unsorted characters, finds the largest set of anagrams in a dictionary
  * 
